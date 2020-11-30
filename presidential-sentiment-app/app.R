a418586ff86d309545dd1875050586a937624ab0
zeinhorn <34073291+zeinhorn@users.noreply.github.com>
2020-11-30 15:06:48 -0500

    gather urls

--- presidential-sentiment-app/app.R
+++ presidential-sentiment-app/app.R
@@ -1,6 +1,13 @@
 #presidents
 
 library(shiny)
+library(tidyverse)
+library(Rcrawler)
+
+Rcrawler("https://millercenter.org/the-presidency/presidential-speeches")
+
+filtered <- INDEX %>% filter(str_detect(Url, "https://millercenter.org/the-presidency/presidential-speeches/.*"))
+filtered <- filtered %>% select(Url)
 
 ui <- fluidPage(
   
