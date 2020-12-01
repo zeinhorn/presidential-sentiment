dd47982df4448f3391db0aa36aed93d34545077b
zeinhorn <34073291+zeinhorn@users.noreply.github.com>
2020-12-01 17:13:08 -0500

    filter speech crawls

--- presidential-sentiment-app/app.R
+++ presidential-sentiment-app/app.R
@@ -4,11 +4,24 @@ library(shiny)
 library(tidyverse)
 library(Rcrawler)
 
-Rcrawler("https://millercenter.org/the-presidency/presidential-speeches")
+Rcrawler(Website = "https://millercenter.org/the-presidency/presidential-speeches", dataUrlfilter = "https://millercenter.org/the-presidency/presidential-speeches/.*")
 
 filtered <- INDEX %>% filter(str_detect(Url, "https://millercenter.org/the-presidency/presidential-speeches/.*"))
 filtered <- filtered %>% select(Url)
 write.csv(filtered, "/Users/zeinhorn/Documents/school/college/senior/ds/Final_project/presidential-sentiment/speech-urls.csv")
+
+speech_urls <- read_csv("/Users/zeinhorn/Documents/school/college/senior/ds/Final_project/presidential-sentiment/speech-urls.csv")
+
+filtered2 <- INDEX %>% filter(str_detect(Url, "https://millercenter.org/the-presidency/presidential-speeches/.*"))
+filtered2 <- filtered2 %>% select(Url)
+write.csv(filtered2, "/Users/zeinhorn/Documents/school/college/senior/ds/Final_project/presidential-sentiment/speech-urls2.csv")
+
+speech_urls2 <- read_csv("/Users/zeinhorn/Documents/school/college/senior/ds/Final_project/presidential-sentiment/speech-urls2.csv")
+
+filtered3 <- INDEX %>% filter(str_detect(Url, "https://millercenter.org/the-presidency/presidential-speeches/.*"))
+filtered3 <- filtered3 %>% select(Url)
+write.csv(filtered3, "/Users/zeinhorn/Documents/school/college/senior/ds/Final_project/presidential-sentiment/speech-urls3.csv")
+
 
 ui <- fluidPage(
   
