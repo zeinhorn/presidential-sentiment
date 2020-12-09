eb4b8712e709efae170d2031a03153d3eeb77869
maerey32 <mereynolds@middlebury.edu>
2020-12-09 15:29:31 -0500

    conclusion

--- presidential-sentiment-app/app.R
+++ presidential-sentiment-app/app.R
@@ -11,6 +11,7 @@ library(dplyr)
 library(tidytext)
 library(lubridate)
 library(ggrepel)
+library(shinycssloaders)
 
 #Reading in and preparing speech + political party data
 speech.party <- read_csv("speech_party_summary.csv") 
@@ -51,10 +52,36 @@ speech.by.pres <- speech.by.pres %>%
   select(name, president.sentiment) %>%
   distinct()
 
+#Making new data frames with highest and lowest average sentiment values
+pres.high.5 <- speech.by.pres %>%
+  arrange(desc(president.sentiment)) %>%
+  head(5)
+pres.low.5 <- speech.by.pres %>%
+  arrange(president.sentiment) %>%
+  head(5)
+
+#Finding average sentiment for each political party
+speech.party.2 <- speech.party %>%
+  select(sentiment.value, Party) %>%
+  group_by(Party) %>%
+  filter(!is.na(sentiment.value)) %>%
+  mutate(party.sentiment = mean(sentiment.value)) %>%
+  mutate(min.party.sentiment = min(sentiment.value)) %>%
+  mutate(max.party.sentiment = max(sentiment.value)) %>%
+  mutate(med.party.sentiment = median(sentiment.value))
+speech.party.final <- speech.party.2 %>%
+  select(Party, party.sentiment, min.party.sentiment, max.party.sentiment,
+         med.party.sentiment) %>%
+  distinct() %>%
+  arrange(desc(party.sentiment))
+
 ui <- navbarPage("Sentiment Analysis of US Presidents' Speeches",
                  tabPanel("Introduction",
-                          textOutput(outputId = "intro")),
+                          verbatimTextOutput(outputId = "intro")),
                  tabPanel("Graph",
+                          
+                          #Directions for the graph
+                          verbatimTextOutput(outputId = "directions"),
                           
                           #Choosing to color the graph by president or by political party
                           selectizeInput(inputId = "var1",
@@ -75,24 +102,67 @@ ui <- navbarPage("Sentiment Analysis of US Presidents' Speeches",
                                               brush = brushOpts(
                                                 id = "plot2_brush",
                                                 resetOnNew = TRUE
-                                              ))),
+                                              )) %>% withSpinner(color = "lightblue")),
                           fluidRow(plotOutput("plot3", height = 600, brush = "plot3_brush")),
                           fluidRow(
                             verbatimTextOutput(outputId = "info", placeholder = TRUE)
                           )
                  ),
                  tabPanel("Table",
+                          verbatimTextOutput(outputId = "results"),
                           selectizeInput(inputId = "var3",
                                          label = "Choose Two or More Presidents' Sentiment Values to Compare",
                                          choices = levels(factor(speech.by.pres$name)),
                                          multiple = TRUE,
                                          selected = "George Washington"),
-                          tableOutput(outputId = "table")),
+                          tableOutput(outputId = "interactive.table"),
+                          textOutput(outputId = "toptitle"),
+                          tableOutput(outputId = "topfive"),
+                          textOutput(outputId = "bottomtitle"),
+                          tableOutput(outputId = "bottomfive"),
+                          textOutput(outputId = "partytitle"),
+                          tableOutput(outputId = "partysentiment")),
                  tabPanel("Conclusion",
-                          textOutput(outputId = "conclusion"))
+                          verbatimTextOutput(outputId = "conclusion"))
 )
 
 server <- function(input, output, session) {
+  
+  output$intro <- renderText({
+    "Maggie Reynolds, Mia Kojima, Zack Einhorn
+   MATH0216 - Final Project
+   12/09/20
+   
+   The data set used consists of all U.S. presidents by name, all their speeches,
+   the date the speeches were given, the average afinn sentiment value of the speeches, 
+   the presidents' political party, and the presidents' overall average sentiment score.
+   
+   The goals of this report are to compare how positive or negative different presidents' 
+   speeches were over time, identify the overall most positive and negative presidents, 
+   recognize any relation between political party and average afinn score (if any), 
+   and observe how significant events may have shifted speech sentiment.
+   
+   'The AFINN lexicon is a list of English terms manually rated for valence with 
+   an integer between -5 (negative) and +5 (positive) by Finn Årup Nielsen between 
+   2009 and 2011.'
+    
+   Finn Årup Nielsen, “A new ANEW: evaluation of a word list for sentiment analysis 
+    in microblogs”, Proceedings of the ESWC2011 Workshop on ‘Making Sense of Microposts’:
+    Big things come in small packages. Volume 718 in CEUR Workshop Proceedings: 93-98.
+    2011 May. Matthew Rowe, Milan Stankovic, Aba-Sah Dadzie, Mariann Hardey (editors)"
+   })
+    
+  output$directions <- renderText({
+    "     Directions: Highlight sections of the first graph with your mouse to see a 
+     zoomed in image on the second graph. Highlight sections of the second graph 
+     to see the president's name, the date the speech was given, and a summary.
+     
+     Optional: Choose to color the graph by either presidents' names or 
+     political parties. Choose to add a max of 2 events to the graphs.
+     
+     Note: There are only 44 names listed due to Grover Cleveland serving two 
+     nonconsecutive terms. The speeches from both terms are combined under his name."
+  })
   
   ranges2 <- reactiveValues(x = NULL, y = NULL)
   
@@ -157,7 +227,7 @@ server <- function(input, output, session) {
       min <- floor(as.numeric(input$plot3_brush$xmin))
       max <- floor(as.numeric(input$plot3_brush$xmax))
     }
-    poi <- speech.party %>% filter(date > min & date < max)
+    poi <- speech.party %>% filter(date > min & date < max) %>% arrange(date)
     
     
     #Making for loop to display date and summary for person of interest (poi)
@@ -174,13 +244,73 @@ server <- function(input, output, session) {
     })
   })
   
+  output$results <- renderText({
+    "     This page includes a quantitative analysis by ranking individual presidents
+    by sentiment value and ranking average political party sentiment value.
+    We also created a tool for users to compare the average sentiment values
+    of individual presidents of their choice."
+  })
   #Making a table of each president's average sentiment
-  output$table <- renderTable({
-    presChoice <-input$var3
-    print(presChoice)
-    presselected <- speech.by.pres %>%
-      filter(c(name == presChoice))
-    print(presselected)
+  output$interactive.table <- renderTable({
+    speech.by.pres %>%
+      filter(c(name %in% input$var3)) %>%
+      arrange(president.sentiment) %>%
+      rename("Name" = "name",
+             "Sentiment Value" = "president.sentiment")
+  })
+  
+  #Making a title for highest presidents' sentiments
+  output$toptitle <- renderText({
+    "Presidents With Five Highest Average Sentiment Values"
+  })
+  
+  #Making a table of highest and lowest five president's sentiments
+  output$topfive <- renderTable({
+    pres.high.5 %>%
+      rename("Name" = "name",
+             "Sentiment Value" = "president.sentiment")
+  })
+  
+  #Making a title for lowest presidents' sentiments
+  output$bottomtitle <- renderText({
+    "Presidents With Five Lowest Average Sentiment Values"
+  })
+  
+  output$bottomfive <- renderTable({
+    pres.low.5 %>%
+      rename("Name" = "name",
+             "Sentiment Value" = "president.sentiment")
+  })
+  
+  #Making a title and table for average sentiment by party
+  output$partytitle <- renderText({
+    "Average Sentiment Value of Each Political Party"
+  })
+
+    output$partysentiment <- renderTable({
+      speech.party.final %>%
+        rename("Political Party" = "Party",
+               "Sentiment Value" = "party.sentiment",
+               "Minimum Party Sentiment" = "min.party.sentiment",
+               "Median Party Sentiment" = "med.party.sentiment",
+               "Maximum Party Sentiment" = "max.party.sentiment")
+    })
+
+  
+  
+  #Conclusion statement
+  output$conclusion <- renderText({
+    "     Each president has a different personality, speaking style, and brand.
+    However, based on our examination of each president's sentiment value and the 
+    impact of major events on their sentiment value, we concluded that major events 
+    have a greater influence on sentiment value than a president's disposition does. 
+    For example, Lincoln and FDR were president through two of the most stressful 
+    periods in our country's history and had two of the lowest sentiment values. 
+    
+    The presidents with the most positive and negative sentiment values were all
+    over 75 years ago; FDR is the most recent president in either the five highest or 
+    five lowest sentiment values. This suggests that presidents have become
+    less emotional and extreme in their style of speeches. "
     
   })
 }
