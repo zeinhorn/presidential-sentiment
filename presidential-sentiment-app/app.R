76d63931fcd96c86a064d47b581ed58ae4ef2abf
maerey32 <mereynolds@middlebury.edu>
2020-12-09 11:37:37 -0500

    naming and mutating avg.sent column

--- presidential-sentiment-app/app.R
+++ presidential-sentiment-app/app.R
@@ -1,7 +1,6 @@
 #presidents
 
-#install.packages("SentimentAnalysis")
-
+#Loading libraries
 library(shiny)
 library(tidyverse)
 library(Rcrawler)
@@ -13,53 +12,54 @@ library(tidytext)
 library(lubridate)
 library(ggrepel)
 
+#Reading in and preparing speech + political party data
 speech.party <- read_csv("speech_party_summary.csv") 
 speech.party <- speech.party [ ,-1] %>%
   distinct()
 speech.party$date <- strptime(as.character(speech.party$date), "%B %d, %Y")
 speech.party$date <- as.Date(speech.party$date)
 
+#Graph of speech + political party 
 speech.party %>%
   ggplot(aes(x = date,
              y = sentiment.value)) +
   geom_col(aes(color = name))
 
-#gdp.growth <- read_csv("GDP_Growth.csv")
-#gdp.growth$Date <- strptime(as.character(gdp.growth$Date), "%Y")
-#gdp.growth$Date <- as.Date(gdp.growth$Date)
-
-speech.party %>%
-  ggplot() +
-  geom_col(data= speech.party,
-           aes(x= date,
-               y= sentiment.value,
-               color = name)) +
-  geom_point(data = events,
-             aes(x=Year,
-                 y=0))
-
-
-#Major evemts
+#Reading in and preparing major events data
 events <- read_csv("Major_events.csv")
 events$Year <- strptime(as.character(events$Year), "%F")
 events$Year <- as.Date(events$Year)
-
 events.test <- events[1:2, ]
 
-
+#Graph of speech/political party with major events
+speech.party %>%
+  ggplot() +
+  geom_col(data= speech.party,
+           aes(x = date,
+               y = sentiment.value,
+               color = name)) +
+  geom_point(data = events,
+             aes(x = Year,
+                 y = 0))
 
 ui <- fluidPage(
+  
+  #Choosing to color the graph by president or by political party
   selectizeInput(inputId = "var1",
                  label = "Choose What to Color By",
                  choices = c("President" = "name",
                              "Political Party" = "Party"),
                  selected = "name"),
+  
+  #Choosing what events to show on the graph
   selectizeInput(inputId = "var2",
-                 label = "Choose a max of two major events to show",
+                 label = "Choose up to Two Major Events to Show",
                  multiple=TRUE,
                  choices = levels(factor(events$Event)),
                  selected = "Declaration of Independence"),
-  fluidRow(h4("Top plot controls bottom plot")),
+  
+  #Creating the two graphs - second one is the first zoomed in on one part
+  fluidRow(h4("The Top Plot Controls The Bottom Plot")),
   fluidRow(plotOutput("plot2", height = 600,
                       brush = brushOpts(
                         id = "plot2_brush",
@@ -69,43 +69,42 @@ ui <- fluidPage(
   fluidRow(
     verbatimTextOutput(outputId = "info", placeholder = TRUE)
   )
-  
 )
 
 server <- function(input, output, session) {
+  
   ranges2 <- reactiveValues(x = NULL, y = NULL)
   
+  #Setting up for graph of speeches and selected events 
   output$plot2 <- renderPlot({
-    
     cdChoice <-input$var2
-    
     eventselected <- events %>%
       filter(Event == cdChoice)
     
-    
+  #Creating graph of speeches and selected events
     ggplot(speech.party, aes(x = date,
                              y = sentiment.value)) +
       geom_col(aes_string(color = input$var1)) +
       geom_point(data = eventselected,
-                 aes(x= Year, y=0))+
+                 aes(x = Year, y = 0))+
       xlab("Year") +
       ylab("Sentiment Value") +
       ggtitle("President Speeches Over Time")
   })
   
+  #Setting up for the zoomed in graph of speeches and selected events
   output$plot3 <- renderPlot({
-    
     cdChoice <-input$var2
-    
     eventselected <- events %>%
       filter(Event == cdChoice)
     
+    #Creating zoomed in graph of speeches and selected events
     ggplot(speech.party, aes(x = date,
                              y = sentiment.value)) +
       geom_col(aes_string(color = input$var1),
                show.legend = FALSE) +
       geom_point(data = eventselected,
-                 aes(x= Year, y=0))+
+                 aes(x = Year, y = 0))+
       xlab("Year") +
       ylab("Sentiment Value") +
       ggtitle("Zoomed In: President Speeches Over Time") +
